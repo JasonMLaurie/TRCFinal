bfbe0885073194791d535183de587f363c45a26d
Ömer Aras Kaplan <omeraraskaplan@gmail.com>
2022-04-28 10:44:10 +0300

    tablayout, fab hide extension

--- app/src/main/java/com/example/tripplanner/Controller/bll/UILogic.kt
+++ app/src/main/java/com/example/tripplanner/Controller/bll/UILogic.kt
@@ -1,16 +1,15 @@
 package com.example.tripplanner.Controller.bll
 
 import androidx.core.view.isVisible
+import com.example.tripplanner.databinding.ActivityMainBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.tabs.TabLayout
 
 class UILogic {
     companion object{
-        fun TabLayout.hide(boolean: Boolean){
-            this.isVisible=!boolean
-        }
-        fun FloatingActionButton.hide(boolean: Boolean){
-            this.isVisible=!boolean
+        fun ActivityMainBinding.hideTabLayout(boolean: Boolean){
+            this.tabLayout.isVisible=!boolean
+            this.fabYerEkle.isVisible=!boolean
         }
     }
 }
